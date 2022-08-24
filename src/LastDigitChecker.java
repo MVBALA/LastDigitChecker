d3aff7e03a922a9ab267e5d25481cf0d447ab04a
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-24 19:22:59 +0530

    it is an udemy exercise

---
+++ src/LastDigitChecker.java
@@ -0,0 +1,17 @@
+public class LastDigitChecker {
+    public static boolean hasSameLastDigit(int a,int b,int c) {
+        if (isValid(a)&&isValid(b)&&isValid(c)){
+            if((a%10==b%10)||(a%10==c%10)||(c%10==b%10)){
+                return true;
+            }else
+                return false;
+        }else
+            return false;
+    }
+    public static boolean isValid(int a){
+        if(a>=10 && a<=1000)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LastDigitChecker.hasSameLastDigit(22,13,23));
+        System.out.println(LastDigitChecker.hasSameLastDigit(223,133,4));
+        System.out.println(LastDigitChecker.hasSameLastDigit(225,135,236));
+        System.out.println(LastDigitChecker.hasSameLastDigit(2265,136,235));
+    }
+}
